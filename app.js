c44fb60d35a7229da520f42a44a30ed0cb886dc3
anasnahil <nahil.anas1@gmail.com>
2021-06-15 21:11:44 -0400

    finished the backend

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const transactionsController = require("./controllers/TransactionsController")
 const cors = require('cors');
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  console.log("This code runs for every request");
+  next();
+});
+
 
 // ROOT
 app.get("/", (req, res) => {
-  res.send("Basic Express App - ROOT");
+  res.send("Welcome To Your Transactions account");
 });
 
+app.use('/transactions', transactionsController);
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
 module.exports = app;
---
+++ controllers/TransactionsController.js
@@ -0,0 +1,41 @@
+const transactions = require('express').Router();
+const transactionArray = require('../models/Transaction')
+
+transactions.get('/', (req, res) => {
+    console.log(res)
+    res.json(transactionArray)
+})
+transactions.get("/:id", (req, res) => {
+    const { id } = req.params;
+    if (transactionArray[id]) {
+      res.json(transactionArray[id]);
+    } else {
+      res.redirect("/404");
+    }
+});
+
+transactions.post('/', (req, res) => {
+    transactionArray.push(req.body);
+    res.json(transactionArray[transactionArray.length - 1])
+})
+
+transactions.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    if (transactionArray[id]) {
+        const deletedTransaction = transactionArray.splice(id, 1);
+        res.status(200).json(deletedTransaction)
+    } else {
+        res.redirect('/404')
+    }
+})
+transactions.put('/:id', (req, res) => {
+    const { id } = req.params;
+    if (transactionArray[id]) {
+        transactionArray[req.params.id] = req.body;
+    res.status(200).json(transactionArray[req.params.id]);
+    } else {
+        res.redirect('/404')
+    }
+    
+})
+module.exports = transactions
